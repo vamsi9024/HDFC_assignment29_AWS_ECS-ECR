15267974aebf6ad184ff10201539989df8352ee8
vamsi9024 <petavamsikrishna9024@gmail.com>
2025-11-06 15:11:10 +0530

    commited

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# ===== Build Stage =====
+FROM maven:3.9.6-eclipse-temurin-21 AS build
+WORKDIR /app
+COPY . .
+RUN mvn clean package -DskipTests
+
+# ===== Run Stage =====
+FROM eclipse-temurin:21-jdk
+WORKDIR /app
+COPY --from=build /app/target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/vamsi/awsdemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.vamsi.awsdemo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to AWS ECR-ECS Page";
+    }
+}
